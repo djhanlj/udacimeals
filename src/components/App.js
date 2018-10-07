add46476cfdd1826e63042c0e0ec70d61e0a3844
Djair <djhanlj@gmail.com>
2018-10-07 18:24:27 -0300

    implementação do Provider da biblioteca react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import '../App.css';
 
 class App extends Component {
-
-
   render() {
     return (
-      <div>
-        Hello World
+      <div className="App">
+       Hello Word
       </div>
     );
   }
